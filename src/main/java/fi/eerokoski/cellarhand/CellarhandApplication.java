b818ee76798304b64e2dc3419c975d0dc9308ee0
EeroKoski <bfr563@myy.haaga-helia.fi>
2021-03-17 17:55:46 +0200

    eka

---
+++ src/main/java/fi/eerokoski/cellarhand/CellarhandApplication.java
@@ -0,0 +1,13 @@
+package fi.eerokoski.cellarhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellarhandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CellarhandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/eerokoski/cellarhand/CellarhandApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.eerokoski.cellarhand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CellarhandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
